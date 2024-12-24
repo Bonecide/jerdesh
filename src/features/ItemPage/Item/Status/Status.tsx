4b125a74e2397fa8b78418c3a93e594db4913479
Bonecide <marselkasymov23@gmail.com>
2024-12-24 21:17:30 +0600

    feat: added click on category of item

--- src/features/ItemPage/Item/Status/Status.tsx
+++ src/features/ItemPage/Item/Status/Status.tsx
@@ -1,8 +1,24 @@
-import { Announce } from "@/atoms/announcements";
+"use client";
+import { Announce, announcementsFiltersAtom } from "@/atoms/announcements";
+import { useSetAtom } from "jotai";
+import { useRouter } from "nextjs-toploader/app";
+import { useCallback } from "react";
 
 export const Status = ({ item }: { item: Announce }) => {
+  const setFilters = useSetAtom(announcementsFiltersAtom);
+  const router = useRouter();
+  const onClick = useCallback(() => {
+    setFilters((prev) => ({
+      ...prev,
+      category_id: item.category.id,
+    }));
+    router.push(`/`);
+  }, [item.category.id, router, setFilters]);
   return (
-    <p className="text-center text-primary lg:text-[20px] md:text-[15px]">
+    <p
+      className="text-center text-primary lg:text-[20px] md:text-[15px] cursor-pointer"
+      onClick={onClick}
+    >
       {item.category.title} {item.subcategory && `/ ${item.subcategory.title}`}
     </p>
   );
