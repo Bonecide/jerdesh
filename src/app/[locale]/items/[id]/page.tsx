7c61ad58b79fb6d541ab6d1fd8c81ecdf222e872
Bonecide <marselkasymov23@gmail.com>
2025-02-01 19:08:41 +0600

    feat: enhance error handling for item details retrieval

--- src/app/[locale]/items/[id]/page.tsx
+++ src/app/[locale]/items/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Announce } from "@/atoms/announcements";
 import { Item } from "@/features/ItemPage/Item";
 import { baseGetRequest } from "@/services/utils/rentalFetch/baseGetRequest";
 import { BASE_IMAGE_URL } from "@/utils/const/env";
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface ItemDetailsProps {
@@ -11,35 +12,47 @@ interface ItemDetailsProps {
 }
 
 export async function generateMetadata({ params }: ItemDetailsProps) {
-  let { data } = await baseGetRequest<Announce>(`announcement/${params.id}`, {
-    config: {
-      isServer: true,
-    },
-  });
+  try {
+    let response = await baseGetRequest(`announcement/${params.id}`, {
+      config: { isServer: true },
+    });
 
-  return {
-    title: `${data.title} | Jerdeshkg.ru`,
-    description: data.description,
-    openGraph: {
-      images: [`${BASE_IMAGE_URL + data.images[0].path}`],
-      title: `${data.title} | Jerdeshkg.ru`,
-      description: data.description,
-    },
-  };
+    const announce = response as { data?: Announce };
+    if (!announce.data) throw new Error("Announcement not found");
+
+    return {
+      title: `${announce.data.title} | Jerdeshkg.ru`,
+      description: announce.data.description,
+      openGraph: {
+        images: [`${BASE_IMAGE_URL + announce.data.images[0]?.path}`],
+        title: `${announce.data.title} | Jerdeshkg.ru`,
+        description: announce.data.description,
+      },
+    };
+  } catch {
+    return {
+      title: "Объявление не найдено",
+      description: "К сожалению, запрашиваемое объявление не существует.",
+    };
+  }
 }
 
 const ItemDetails = async ({ params }: ItemDetailsProps) => {
-  let { data } = await baseGetRequest<Announce>(`announcement/${params.id}`, {
-    config: {
-      isServer: true,
-    },
-  });
+  try {
+    let data = await baseGetRequest(`announcement/${params.id}`, {
+      config: { isServer: true },
+    });
+    const announce = data as { data?: Announce };
+    if (!announce.data) return notFound(); // Проверяем `success` и наличие `data`
 
-  return (
-    <div className="containerBlock">
-      <Item item={data} />
-    </div>
-  );
+    return (
+      <div className="containerBlock">
+        <Item item={announce.data} />
+      </div>
+    );
+  } catch {
+    return notFound(); // Ошибка запроса -> 404
+  }
 };
 
 export default ItemDetails;
